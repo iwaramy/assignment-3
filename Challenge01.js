636f325ca777e2ae464b897e99f2d057fe913edd
Yogi <yugeshiwaram@YUGESHs-iMac.local>
2019-02-04 15:25:45 -0800

    code add

---
+++ Challenge01.js
@@ -0,0 +1,12 @@
+let arr = ['laptop', 'desktop', 'mobile', 'ipad', 'tablet'];
+
+console.log(arr);
+
+arr.push('Chair');
+
+console.log(arr);
+arr.splice(3, 1);
+
+console.log(arr);
+let str = arr.join(' , ');
+console.log(str);
