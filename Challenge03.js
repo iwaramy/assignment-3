c962e9431c220507ff7d54d14c529066782a7f24
Yogi <yugeshiwaram@YUGESHs-iMac.local>
2019-02-04 16:11:56 -0800

    code change

---
+++ Challenge03.js
@@ -0,0 +1,4 @@
+let arr = ['Logo Design', 'Brand Identity', 'Website Design', 'Product Photography', 'Packaging Design'];
+for (let i = 0; i < arr.length; i++) {
+  console.log(i + ', ' + arr[i]);
+  }
